b621caff70e4213869de73d7296c4af95dae11c0
mzereba <mzereba@10.153.33.11>
2015-11-22 10:39:06 +0300

    Added username check in signup

--- js/app.js
+++ js/app.js
@@ -1,34 +1,100 @@
 // app module
 var app = angular.module('App', []);
 
-	// home controller
-	app.controller('HomeController', function ($scope) {
-	    $scope.header = "Welcome to Meccano.io";
-	});
-	
-	// about controller
-	app.controller('AboutController', function ($scope) {
-	    $scope.header = "About";
-	});
-	
-	// store controller
-	app.controller('StoreController', function ($scope) {
-	    $scope.header = "Application Store";
-	});
-	
-	//login controller
-	app.controller('LoginController', function ($scope) {
-	    $scope.header = "Login";
-	});
-	
-	//signup controller
-	app.controller('SignupController', function ($scope) {
-	    $scope.header = "Signup";
-	});
-	
-	// post controller
-	app.controller('PostController', function ($scope) {
-	    $scope.header = "Posts";
+app.directive('ngFocus', function($timeout) {
+    return {
+        link: function ( scope, element, attrs ) {
+            scope.$watch( attrs.ngFocus, function ( val ) {
+                if ( angular.isDefined( val ) && val ) {
+                    $timeout( function () { element[0].focus(); } );
+                }
+            }, true);
+
+            element.bind('blur', function () {
+                if ( angular.isDefined( attrs.ngFocusLost ) ) {
+                    scope.$apply( attrs.ngFocusLost );
+
+                }
+            });
+        }
+    };
+});
+
+// home controller
+app.controller('HomeController', function ($scope) {
+    $scope.header = "Welcome to Meccano.io";
+});
+
+// about controller
+app.controller('AboutController', function ($scope) {
+    $scope.header = "About";
+});
+
+// store controller
+app.controller('StoreController', function ($scope) {
+    $scope.header = "Application Store";
+});
+
+// login controller
+app.controller('LoginController', function ($scope) {
+    $scope.header = "Login";
+});
+
+// signup controller
+app.controller('SignupController', function ($scope, $http, $location) {
+    $scope.header = "Signup";
+    $scope.isFocused = true;
+    $scope.error = false;
+    $scope.show = true;
+ 
+    $scope.test = function (account) {
+    	$scope.error = true;
+  	  	$scope.account.user= "";
+  	  	$scope.isFocused = true;
+  	  	//$location.path( "/login" );
+    };
+    
+    // checks if the account name exists
+    $scope.check = function (account) {
+    	var uri = "";
+        $http({
+          method: 'POST', 
+          url: uri,
+          data: account,
+          headers: {
+        	'Content-Type': 'application/json',
+        	'Accept': 'text/html'
+          },
+          withCredentials: true
+        }).
+        success(function(data, status, headers) {
+          if (status == 200 || status == 201) {
+        	  //Account created
+        	  $scope.result = data;
+        	  $location.path(""); 
+          }
+        }).
+        error(function(data, status) {
+          if (status == 401) {
+            console.log('Forbidden', 'Authentication required to create a new account.');
+          } else if (status == 403) {
+        	  console.log('Forbidden', 'You are not allowed to create a new account with this name.');
+        	  //If account name already taken
+        	  $scope.error = true;
+        	  $scope.account.user= "";
+        	  $scope.isFocused = true;
+        	  //$scope.$digest();
+          } else {
+        	  console.log('Failed '+ status + data);
+          }
+        });
+    };
+    
+});
+
+// post controller
+app.controller('PostController', function ($scope) {
+    $scope.header = "Posts";
 
     // define posts json
     $scope.posts =  [{
@@ -62,43 +128,41 @@ var app = angular.module('App', []);
         "intro": "klasjdflk ljdsfalk sdaf",
         "extended": "Integer facilisis aliquet leo, non sodales nibh eleifend ac. Donec risus tellus, porttitor sit amet gravida non, ullamcorper in velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi."
     }];
-
-
 });
 
-	// routes
-	app.config(function ($routeProvider) {
-	
-	   $routeProvider
-	    .when('/',
-	        {
-	            controller: 'HomeController',
-	            templateUrl: 'views/welcome.html'
-	        })
-	    .when('/about',
-	        {
-	            controller: 'AboutController',
-	            templateUrl: 'views/about.html'
-	        })
-	    .when('/store',
-	        {
-	            controller: 'StoreController',
-	            templateUrl: 'views/store.html'
-	        })
-	    .when('/posts',
-	        {
-	            controller: 'PostController',
-	            templateUrl: 'views/posts.html'
-	        })
-	    .when('/login',
-	        {
-	            controller: 'LoginController',
-	            templateUrl: 'views/login.html'
-	        })
-	    .when('/signup',
-	        {
-	            controller: 'SignupController',
-	            templateUrl: 'views/signup.html'
-	        })
-	    .otherwise({ redirectTo: '/' });
-	});
+// routes
+app.config(function ($routeProvider) {
+
+   $routeProvider
+    .when('/',
+        {
+            controller: 'HomeController',
+            templateUrl: 'views/welcome.html'
+        })
+    .when('/about',
+        {
+            controller: 'AboutController',
+            templateUrl: 'views/about.html'
+        })
+    .when('/store',
+        {
+            controller: 'StoreController',
+            templateUrl: 'views/store.html'
+        })
+    .when('/posts',
+        {
+            controller: 'PostController',
+            templateUrl: 'views/posts.html'
+        })
+    .when('/login',
+        {
+            controller: 'LoginController',
+            templateUrl: 'views/login.html'
+        })
+    .when('/signup',
+        {
+            controller: 'SignupController',
+            templateUrl: 'views/signup.html'
+        })
+    .otherwise({ redirectTo: '/' });
+});
